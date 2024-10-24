c788e9f625b0b684c698d49dd0dfccb6a5cafeca
Muhammad Vidi <vidim@impstudio.co.id>
2024-10-24 20:52:25 +0700

    add get profile,update,and delete route

--- crud-manager/router.js
+++ crud-manager/router.js
@@ -1,17 +1,42 @@
 const express = require('express')
 const router = express.Router()
 const controller = require('./controller/controller.js')
-const {validator, validateRequest} = require('./middleware/validator.js')
+const {validatorRegister, validateRequest, validatorLogin} = require('./middleware/validator.js')
+const verifyToken = require('./middleware/jwt.js')
 
 router.get('/hello', (req, res) => {
   res.send('hello api')
 })
 router.post('/auth/register',
-  validator,
+  validatorRegister,
   validateRequest,
   controller.register
 )
 
-router.post('/auth/login', controller.login)
+router.post('/auth/login',
+  validatorLogin,
+  validateRequest, 
+  controller.login
+)
+
+router.get('/auth/find/:id',
+  verifyToken, 
+  controller.getUserById
+)
+
+router.get('/auth/profile', 
+  verifyToken,
+  controller.getProfile
+)
+
+router.patch('/auth/update',
+  verifyToken,
+  controller.updateUser
+)
+
+router.delete('/auth/delete',
+  verifyToken,
+  controller.deleteUser
+)
 
 module.exports = router
